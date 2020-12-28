fb1ccf9bdbb76f7072f299640f5e743eb44786ea
ErickTejaxun <erickteja@gmail.com>
2020-12-27 19:03:19 -0600

    Update scripts

--- microservicio-usuario/routes/index.js
+++ microservicio-usuario/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     let date_ob = new Date();
-    res.send("Probando despliegues automaticos con ansible. Hora del despliegue: " + date_ob.getHours() +"-"+date_ob.getMinutes()+ "-"+date_ob.getSeconds();
+    res.send("Probando despliegues automaticos con ansible. Hora del despliegue:  10.001" );
 });
 
 module.exports = router;
