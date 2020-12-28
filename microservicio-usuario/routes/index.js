6d6b76124958a1f2a1ef23d2c08eabf14d500d03
ErickTejaxun <erickteja@gmail.com>
2020-12-27 19:23:08 -0600

    Update scripts

--- microservicio-usuario/routes/index.js
+++ microservicio-usuario/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     let date_ob = new Date();
-    res.send("Version 0.0.0.12" );
+    res.send("Version 0.0.0.13" );
 });
 
 module.exports = router;
