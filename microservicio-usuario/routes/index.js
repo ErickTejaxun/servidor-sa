dcb7b12ae5b4afffa09b735b0ec8dc5b3ce40ef4
jelp2008 <eliu.lacan@gmail.com>
2020-12-29 16:08:46 -0600

    login cliente

--- microservicio-usuario/routes/index.js
+++ microservicio-usuario/routes/index.js
@@ -7,3 +7,32 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
+/*El login se probo en postman con esta cadena --Eliu
+{
+    "email":"eder@usac.com",
+    "contrasena":"SuperSegura"
+}*/
+router.post('/login-cliente', (req, res) => {
+    const { email, contrasena } = req.body;
+    const sql = `SELECT * FROM Usuario WHERE email='${email}' AND contrasena='${contrasena}'`;
+    console.log(sql);
+    const query = conn.query(sql, (err, results) => {
+        if (err) {
+            console.log(err)
+            console.log("error1")
+            res.send({ auth: false });
+        } else {
+            if (results.length === 1) {
+                //console.log(results[0])
+                res.send({
+                    auth: true,
+                    result: results[0]
+                });
+            } else {
+                console.log("error2")
+                res.send({ auth: false });
+            }
+        }
+    });
+});
